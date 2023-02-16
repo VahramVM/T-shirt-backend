93b407453fb8958c801a38fb0399c6b1efb60e45
VahramVM <vahram.keleshyan@gmail.com>
2023-02-16 16:07:23 +0400

    test17

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ app.use('/api/font', fontRotes);
 app.use('/api/analytics', analyticsRotes);
 
 // if (process.env.NODE_ENV === 'production') {
-    app.use('/static', express.static(path.join(__dirname,  '../t-shirt-frontend', 'dist', 'client')))
+    app.use('/', express.static(path.join(__dirname,  '../t-shirt-frontend', 'dist', 'client')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../t-shirt-frontend', 'dist', 'client', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 't-shirt-frontend', 'dist', 'client', 'index.html'))
     })
 // }
 
